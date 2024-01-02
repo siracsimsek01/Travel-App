2f7da42b0f1a44a00270f30820b7f6304a6028f0
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-01-03 02:00:03 +0500

    update

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-27 lg:py-20 xl:flex-row border-2 border-red-500 '>
+
+    </section>
+  )
+}
+
+export default Hero
