b878bbc567a4e0864a4fa0d78bacbd8f68a51846
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-01-12 21:18:03 +0000

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -23,7 +23,7 @@ const Features = () => {
               height={50}
               className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
               />
-              <h2 className="bold-40 lg:bold-64 ">Our Features!</h2>
+              <h2 className="bold-40 lg:bold-64 ">Our Featuress!</h2>
           </div>
           <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-20">
           {FEATURES.map((feature) => (
