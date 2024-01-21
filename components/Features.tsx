60150810048b998d13020897bd0cb50114fd2091
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-01-21 23:48:18 +0000

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -26,14 +26,14 @@ const Features = () => {
               <h2 className="bold-40 lg:bold-64 ">Our Featuress!</h2>
           </div>
           <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-20">
-          {FEATURES.map((feature) => (
+          {/* {FEATURES.map((feature) => (
               <FeatureItem 
                 key={feature.title}
                 title={feature.title} 
                 icon={feature.icon}
                 description={feature.description}
               />
-            ))}
+            ))} */}
           </ul>
         </div>
       </div>
