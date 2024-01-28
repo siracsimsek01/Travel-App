c57a19880f13bbc90b9a6fad5bce08a271be8e0a
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-01-28 23:37:23 +0000

    update

--- components/Features.tsx
+++ components/Features.tsx
@@ -16,23 +16,23 @@ const Features = () => {
         </div>
         <div className="z-20 flex w-full flex-col lg:w-[60%]">
           <div className="relative ">
-              {/* <Image 
+              <Image 
               src="/camp.svg"
               alt="camp"
               width={50}
               height={50}
               className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
-              /> */}
+              />
               <h2 className="bold-40 lg:bold-64 ">Our Featuress!</h2>
           </div>
           <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-20">
           {FEATURES.map((feature) => (
-              // <FeatureItem 
-              //   key={feature.title}
-              //   title={feature.title} 
-              //   icon={feature.icon}
-              //   description={feature.description}
-              // />
+              <FeatureItem 
+                key={feature.title}
+                title={feature.title} 
+                icon={feature.icon}
+                description={feature.description}
+              />
             ))}
           </ul>
         </div>
